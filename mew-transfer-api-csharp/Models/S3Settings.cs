6f9d7b9cabad72f1fcc2dca121bd4c18df4d88a4
faridasad <asadlifaridd@gmail.com>
2023-05-27 06:12:24 +0400

    Add S3 Settings

---
+++ mew-transfer-api-csharp/Models/S3Settings.cs
@@ -0,0 +1,10 @@
+﻿namespace mew_transfer_api_csharp.Models
+{
+    public class S3Settings
+    {
+        public string AccessKeyId { get; set; } = null!;
+        public string SecretAccessKey { get; set; } = null!;
+        public string BucketName { get; set; } = null!;
+        public string Region { get; set; } = null!;
+    }
+}
