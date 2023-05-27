b4770b6684c11ad804d506ae8e9fb38e143882f5
faridasad <asadlifaridd@gmail.com>
2023-05-27 06:13:10 +0400

    Add Cloudinary Service

---
+++ mew-transfer-api-csharp/Services/CloudinaryService.cs
@@ -0,0 +1,48 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+using mew_transfer_api_csharp.Models;
+using Microsoft.Extensions.Options;
+
+namespace mew_transfer_api_csharp.Services
+{
+    
+
+    public interface ICloudinaryService
+    {
+        Task<string> UploadFile(IFormFile file);
+        // Other methods for image transformations, deletions, etc.
+    }
+
+    public class CloudinaryService : ICloudinaryService
+    {
+        private readonly Cloudinary _cloudinary;
+
+        public CloudinaryService(IOptions<CloudinarySettings> cloudinaryConfig)
+        {
+            var account = new Account(
+                cloudinaryConfig.Value.CloudName,
+                cloudinaryConfig.Value.ApiKey,
+                cloudinaryConfig.Value.ApiSecret);
+
+            _cloudinary = new Cloudinary(account);
+        }
+
+        public async Task<string> UploadFile(IFormFile file)
+        {
+            using var stream = file.OpenReadStream();
+            var uploadParams = new RawUploadParams()
+            {
+                File = new FileDescription(file.Name, stream),
+            };
+
+            var uploadResult = await _cloudinary.UploadAsync(uploadParams);
+
+            Console.WriteLine(uploadResult.Url);
+
+            return uploadResult.Url.ToString();
+        }
+
+        // Implement other methods for image transformations, deletions, etc.
+    }
+
+}
